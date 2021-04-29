ba8cf4d5a6cf48bab86c3bffec4687437e858c10
Chris <cjh232@rutgers.edu>
2021-04-29 19:59:08 -0400

    Cleaned up user view imports

--- MitzysAPI/users/views.py
+++ MitzysAPI/users/views.py
@@ -4,11 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterUserSerializer, UserSerializer
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken, OutstandingToken, BlacklistedToken
 from .models import User, FailedLoginAttempt
 from .utils import *
-from django.contrib.sites.shortcuts import get_current_site
-from django.urls import reverse
 from django.utils import timezone
 import jwt
 from django.conf import settings
